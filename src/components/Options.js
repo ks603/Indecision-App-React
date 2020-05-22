75c1322377b6c6cf6babe5ca32161c8ef12870bd
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-22 16:15:29 -0400

    Change in text

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,22 +6,22 @@ const Options = (props) => (
     <div className="widget-header">
       <h3 className="widget-header__title">Your Options</h3>
       <button
-        className="button button--link" 
+        className="button button--link"
         onClick={props.handleDeleteOptions}>
         Remove All
       </button>
     </div>
-    {props.options.length === 0 && <p className="widget--message">Please add an option to get started!</p>}
-  {
-    props.options.map((option, index) => (
-      <Option 
-      key={option} 
-      optionText={option}
-      count={index + 1}
-      handleDeleteOption={props.handleDeleteOption}
-      />
-    ))
-  }
+    {props.options.length === 0 && <p className="widget--message">Please add an option here to get started!</p>}
+    {
+      props.options.map((option, index) => (
+        <Option
+          key={option}
+          optionText={option}
+          count={index + 1}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
   </div>
 )
 
